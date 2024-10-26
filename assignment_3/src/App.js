974317e51b560511dc03d6a7c51f59a85bb8584d
jeffhhhh <jeffreyshawnremy@gmail.com>
2024-10-26 14:01:03 -0500

    Created basic catalog structure to be displayed on main page

---
+++ assignment_3/src/App.js
@@ -0,0 +1,46 @@
+import React, {useState, useEffect} from 'react';
+import ReactDOM from 'react-dom/client';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container, Card, Col, Button } from 'react-bootstrap';
+
+function BrowseView() {
+    const [catalog, setCatalog] = useState([]);
+    const [filteredCatalog, setFilteredCatalog] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const response = await fetch("/products.json");
+            const data = await response.json();
+            setCatalog(data);
+        }        
+
+        fetchData();
+    }, []);
+
+    return (
+        <div className="d-flex" style={{ height: "100vh" }}>        
+            <div className="flex-grow-1 p-4">
+                <h1>Store Catalog</h1>
+                <div className="row">
+                    {catalog.map((product) => {
+                        return (
+                            <div key={product.id} className="col-md-4">
+                                <div className="card mb-4">
+                                    <div className="card-body">
+                                        <h5 className="card-title">{product.title}</h5>
+                                        <p className="card-text">
+                                            <strong>Price:</strong> ${product.price} <br />
+                                            <strong>Description:</strong> {product.description} <br />
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                        );
+                    })}
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default BrowseView;
