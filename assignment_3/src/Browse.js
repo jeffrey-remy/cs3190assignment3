4a27ed340888b5d68572907f652f03f4134b688d
jeffhhhh <jeffreyshawnremy@gmail.com>
2024-11-03 01:51:04 -0600

    Added search bar and button to catalog page; revised Bootstrap formatting to be more pleasing

--- assignment_3/src/Browse.js
+++ assignment_3/src/Browse.js
@@ -1,5 +1,6 @@
 import React, {useState, useEffect} from "react";
 import "bootstrap/dist/css/bootstrap.css";
+import { Button, FormControl, InputGroup } from "react-bootstrap";
 
 function Browse({cart, setCart, setViewer}) {
     const [catalog, setCatalog] = useState([]);
@@ -9,6 +10,7 @@ function Browse({cart, setCart, setViewer}) {
         const fetchData = async () => {
             const response = await fetch("/products.json");
             const data = await response.json();
+            setFilteredCatalog(data);
             setCatalog(data);
         }        
 
@@ -42,25 +44,66 @@ function Browse({cart, setCart, setViewer}) {
         return matchingProducts.length;
     } 
 
+    // respond to new search input
+    const handleSearch = () => {
+        let input = document.getElementById("searchInput").value;
+        const results = catalog.filter(product => {
+            if (input === "") 
+                return catalog;
+            return product.title.toLowerCase().includes(input.toLowerCase());
+        });
+        // update filtered catalog to search results
+        setFilteredCatalog(results);
+    }
+
+
     return (
-        <div className="d-flex" style={{ height: "100vh" }}>        
-            <div className="flex-grow-1 p-4">
-                <h1>Store Catalog</h1>
-                <button type="button" className="btn btn-primary" onClick={() => onCheckout()}>Checkout</button>
-                <div className="row">
-                    {catalog.map((product) => {
+        <div className="d-flex m-4 justify-content-center">   
+            <div className="mb-4">  
+                <div className="d-flex justify-content-center">
+                    <h1>Store Catalog</h1>  
+                </div>
+                {/* Top bar for searchbar and checkout button */}
+                <nav className="navbar navbar-light bg-light ">
+                    <div className="mx-3 my-2 form-group">
+                        <form className="row g-3">
+                            <div className="col-auto">
+                                <input id="searchInput" className="form-control" type="text" placeholder="Search products"/>
+                            </div>
+                            <div className="col-auto">
+                                <button type="button" className="btn btn-outline-success form-control" onClick={() => handleSearch()}>Search</button>
+                            </div>
+                        </form>
+                    </div>
+                    <div className="mx-3">
+                        <button type="button" className="btn btn-primary" onClick={() => onCheckout()}>Checkout</button>
+                    </div>
+                </nav>
+                <div className="row mt-2" style={{width: "75vw"}}>
+                    {filteredCatalog.map((product) => {
                         return (
-                            <div key={product.id} className="col-md-4">
+                            <div key={product.id} className="col-lg-3 col-md-4">
                                 <div className="card mb-4">
+                                    <img
+                                        src={product.image}
+                                        className="card-img-top"
+                                        style={{ width: "200px", margin: "auto"}}
+                                        alt={product.title}
+                                    />
                                     <div className="card-body">
-                                        <h5 className="card-title">{product.title}</h5>
+                                        <div class="row g-2">
+                                            <h5 className="col-auto card-text" style={{backgroundColor: "green", color: "white", borderRadius: "40%"}}>${product.price}</h5>
+                                            <h5 className="col-auto card-title">{product.title}</h5>
+                                        </div>
                                         <p className="card-text">
-                                            <strong>Price:</strong> ${product.price} <br />
-                                            <strong>Description:</strong> {product.description} <br />
+                                            {product.description} <br />
                                         </p>
-                                        <p>Quantity: {getQuantity(product)}</p>
-                                        <button type="button" className="btn btn-secondary" onClick={() => addToCart(product)}>+</button>
-                                        <button type="button" className="btn btn-secondary" onClick={() => removeFromCart(product)}>-</button>
+                                        <InputGroup>
+                                            <input className="form-control" style={{backgroundColor : "lightgray"}} type="text" placeholder={getQuantity(product)} readOnly />
+                                            <button type="button" className="btn btn-outline-success" onClick={() => addToCart(product)}>+</button>
+                                            <button type="button" className="btn btn-outline-success" onClick={() => removeFromCart(product)}>-</button>
+                                        </InputGroup>
+                                        
                                     </div>
                                 </div>
                             </div>
