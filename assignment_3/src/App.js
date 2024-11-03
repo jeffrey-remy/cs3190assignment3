5e4c8c21e95243124ef3dfdd5c0eae0ba859583b
meyerryan <meyerryan97@gmail.com>
2024-11-03 12:53:24 -0600

    implimented Confirmation screen

--- assignment_3/src/App.js
+++ assignment_3/src/App.js
@@ -10,12 +10,13 @@ import Confirmation from './Confirmation';
 function App() {
     const [cart, setCart] = useState([]);
     const [viewer, setViewer] = useState(0);
+    const [data,setData] = useState([]);
 
     return (
         <div>
             {viewer === 0 && <Browse cart={cart} setCart={setCart} setViewer={setViewer}/>}
-            {viewer === 1 && <Cart cart={cart} setCart={setCart} setViewer={setViewer}/>}
-            {viewer === 2 && <Confirmation cart={cart} setCart={setCart} setViewer={setViewer}/>}
+            {viewer === 1 && <Cart cart={cart} setData={setData} setViewer={setViewer}/>}
+            {viewer === 2 && <Confirmation cart={cart} setViewer={setViewer} data={data}/>}
         </div>
     )
 }
--- assignment_3/src/Confirmation.js
+++ assignment_3/src/Confirmation.js
@@ -1,10 +1,118 @@
-import React, {useState, useEffect} from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+import Button from 'react-bootstrap/Button';
+import "bootstrap/dist/css/bootstrap.css";
+import ListGroup from 'react-bootstrap/ListGroup';
+import Table from 'react-bootstrap/Table';
+import Card from 'react-bootstrap/Card';
 
-function Confirmation({cart, setCart, setViewer}) {
+
+
+function Confirmation({ cart, setViewer, data }) {
+
+
+    console.log(data)
+
+    let sum = 0;
+
+    const returnToCart = () => {
+        setViewer(1);
+    }
+
     return (
-        <div>
-            <h1>Confirmation</h1>
+
+        <div className="d-flex" style={{ height: "100vh" }}>
+            <div className="flex-grow-1 p-4">
+                <Button variant="secondary" onClick={() => {
+                    console.log("pressed return");
+                    returnToCart();
+                }
+                }>← Return</Button>{console.log("Testing Secondary")}
+                <Table striped>
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>First Name</th>
+                            <th>Last Name</th>
+                            <th>Username</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {cart.map((product) => {
+                            sum += product.price;
+                            return (
+                                <tr>
+                                    <td>{product.id}</td>
+                                    <td>{product.title}</td>
+                                    <td>{product.description}</td>
+                                    <td>{product.price}</td>
+                                </tr>
+                            )
+                        })}
+                    </tbody>
+                    <tfoot>
+                        <th></th>
+                        <th>Total</th>
+                        <th>{sum.toFixed(2)}</th>
+                        <th>Total w/tax: {(sum * 1.07).toFixed(2)}</th>
+                    </tfoot>
+                </Table>
+
+                <h1 style={{ color: 'green' }}>Thanks for your buisness {data.fullName}, Order Confirmed!</h1>
+
+                <div style={{ display: 'flex', gap: '1rem' }}>
+                <Card style={{ width: '30rem' }}>
+                    <Card.Body>Order Info
+                        <ul>
+                            <li><strong>Name: </strong>{data.fullName}</li>
+                            <li><strong>Email: </strong>{data.email}</li>
+                            <li><strong>Card Info: </strong>{data.creditCard}</li>
+                            <li><strong>Address: </strong>{data.address}</li>
+                            <li><strong>Apartment, studio, or floor: </strong>{data.address2}</li>
+                            <li><strong>City: </strong>{data.city}</li>
+                            <li><strong>zip:</strong> {data.zip}</li>
+                        </ul>
+                    </Card.Body>
+                </Card>
+                <Card style={{ width: '30rem' }}>
+                    <Card.Body>Cost break down
+                        <Table striped>
+                            <thead>
+                                <tr>
+                                    <th>Item</th>
+                                    <th>Price</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {cart.map((product) => {
+                                    sum += product.price;
+                                    return (
+                                        <tr>
+                                            <td>{product.title}</td>
+                                            <td>{product.price}</td>
+                                        </tr>
+                                    )
+                                })}
+                                <tr>
+                                    <td>Tax</td>
+                                    <td>7%</td>
+                                </tr>
+                            </tbody>
+                            <tfoot>
+                                <th>Total</th>
+                                {/* <th>{sum.toFixed(2)}</th> */}
+                                <th>{(sum * 1.07).toFixed(2)}</th>
+                            </tfoot>
+                        </Table>
+                    </Card.Body>
+                </Card>
+                </div>
+
+                
+
+
+                
+            </div>
         </div>
     )
 }
